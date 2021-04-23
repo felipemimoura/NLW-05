bfdc6a1fae327179eaecee9d078e18652211d106
felipemimoura <felipemimoura@gmail.com>
2021-04-23 13:42:36 -0300

    aula 04 problema com usar id

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,5 @@
-import express from 'express'
-import "./database" //Realizando a conexão com o banco
-import { routes } from './routes'
+import { http } from "./http";
+import "./websocket/client"
 
-const app = express()
 
-app.use(express.json())
-
-app.use(routes)
-
-app.listen(3001, () => console.log("Server is running on port 3001"))
+http.listen(3001, () => console.log("Server is running on port 3001"))
